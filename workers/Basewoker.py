9b8de82a2161331a46229dfd7fbd8b630ede9256
xhl <59420xhl>
2019-07-02 05:57:09 +0000

    add micro extractions

--- workers/Basewoker.py
+++ workers/Basewoker.py
@@ -48,9 +48,8 @@ class BaseWorker(metaclass=abc.ABCMeta):
         df.to_csv(os.path.join(micro_path, '{}.csv'.format(t)),
                   index=None, header=None, encoding='utf-8')
 
-    @property
-    def datest_files(self):
-        path = self.get_logfiles(os.path.join(os.getcwd(), 'data/log'), [])
+    def datest_files(self, path):
+        path = self.get_logfiles(os.path.join(os.getcwd(), path), [])
         if sys.platform in ['win32', 'win64', 'cygwin']:
             path = [_ for _ in path if _.split('\\')[-1].startswith(type(self).__name__.lower())]
             path = sorted(path, key=lambda s: s.split('\\')[-1], reverse=True)[:self.times]
@@ -61,7 +60,7 @@ class BaseWorker(metaclass=abc.ABCMeta):
 
     def read_from_file(self, mode=list):
         logs = []
-        files = self.datest_files
+        files = self.datest_files('data/log')
         try:
             for _ in files:
                 with open(_, 'r') as f:
@@ -92,3 +91,6 @@ class BaseWorker(metaclass=abc.ABCMeta):
 
     def status(self, t):
         print('task {} done, Status:Sucess'.format(t))
+
+    def merge(self):
+        pass

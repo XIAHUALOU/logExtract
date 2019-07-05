d10afd977dfa37194914d94f7943fbb127d8b648
xhl <59420xhl>
2019-07-05 07:40:36 +0000

    add merge to csv function

--- workers/Openjdk.py
+++ workers/Openjdk.py
@@ -1,33 +1,26 @@
 # -*- encoding: utf-8 -*-
 from .Basewoker import BaseWorker
 
+
 class Openjdk(BaseWorker):
     def run(self):
         logs = self.read_from_file(mode=list)
         for log in logs:
             t, log = log
-            patt=r'Score'
+            patt = r'Score'
             pattern = self.re.compile(patt)
-            test_list=[]
+            test_list = []
+            s1_list = []
+            s2_list = []
             for line in log:
-                result = self.re.findall(pattern,line)
-                if(result):
-                    test_list.append(log[log.index(line)+1])
-            s1_list=[]
-            s2_list=[]
-            s1_list.append(test_list[0].split()[3])
-            s1_list.append(test_list[0].split()[5])
-            s2_list.append(test_list[1].split()[3])
-            s2_list.append(test_list[1].split()[5])
+                result = self.re.findall(pattern, line)
+                if (result):
+                    test_list.append(log[log.index(line) + 1])
+            list(map(lambda x: s1_list.append(test_list[0].split()[x]), [3, 5]))
+            list(map(lambda x: s2_list.append(test_list[1].split()[x]), [3, 5]))
             self.merge(s1_list)
             self.merge(s2_list)
             self.status(t)
-        
-
-
-
-
-            
 
     def to_excel(self):
         pass
--- workers/Ruby.py
+++ workers/Ruby.py
@@ -1,52 +1,39 @@
 # -*- encoding: utf-8 -*-
 from .Basewoker import BaseWorker
 
+
 class Ruby(BaseWorker):
     def run(self):
-        logs = self.read_from_file(mode=list) 
+        logs = self.read_from_file(mode=list)
         for log in logs:
-            t, log = log    
-            pattern_r=self.re.compile(r' s -  ')
-            test_list=[]
+            t, log = log
+            pattern_r = self.re.compile(r' s -  ')
+            test_list = []
+            list1 = []
+            list2 = []
             for line in log:
-                target=self.re.findall(pattern_r,line)
-                if(target):
+                target = self.re.findall(pattern_r, line)
+                if (target):
                     test_list.append(line)
-            list1=[]
-            list2=[]
-            #print(test_list)
             for each in test_list:
-                s1=each.split(' s -')[0]
+                s1 = each.split(' s -')[0]
                 list1.append(s1[:-7])
                 list2.append(s1[-7:])
-            s1=self.pd.Series(self.np.array(list1))
-            s2=self.pd.Series(self.np.array(list2))
-            df=self.pd.DataFrame({"name":s1,"result":s2})
-            miss_list=['so_k_nucleotidepreparing /tmp/fasta.output.100000','so_reverse_complementpreparing /tmp/fasta.output.2500000']
-            df['name'][111]=miss_list[0]
-            df['name'][124]=miss_list[1]
-            df['name'][393]=miss_list[0]
-            df['name'][406]=miss_list[1]
-            df1=df[:282]
-            df2=df[282:]
-            df1_copy=df1.drop_duplicates(subset='name', keep='last', inplace=False)
-            df2_copy=df2.drop_duplicates(subset='name', keep='last', inplace=False)
-            res_ruby=self.pd.DataFrame(columns=["score1","score2"],index=df2["name"].unique())
-            list1=list(df1_copy["result"])
-            list2=list(df2_copy["result"])
-            res_ruby["score1"]=list1
-            res_ruby["score2"]=list2
-            #print(list1)
+            s1 = self.pd.Series(self.np.array(list1))
+            s2 = self.pd.Series(self.np.array(list2))
+            df = self.pd.DataFrame({"name": s1, "result": s2})
+            miss_list = ['so_k_nucleotidepreparing /tmp/fasta.output.100000',
+                         'so_reverse_complementpreparing /tmp/fasta.output.2500000']
+            for tup in [(111, 0), (124, 1), (393, 0), (406, 1)]:
+                df['name'][tup[0]] = miss_list[tup[1]]
+            df1, df2 = df[:282], df[282:]
+            df1_copy = df1.drop_duplicates(subset='name', keep='last', inplace=False)
+            df2_copy = df2.drop_duplicates(subset='name', keep='last', inplace=False)
+            list1 = list(df1_copy["result"])
+            list2 = list(df2_copy["result"])
             self.merge(list1)
             self.merge(list2)
-            self.status(t)   
-                
-        
-
-
-
-
-            
+            self.status(t)
 
     def to_excel(self):
         pass

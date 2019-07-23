abe7287dacfd45db9ff860dd7a54c0eadd8e8838
XIAHUALOU <59420xhl>
2019-07-23 19:19:10 +0800

    make code better

---
+++ threadsPool/t_pool.py
@@ -0,0 +1,134 @@
+import threading
+import contextlib
+from queue import Queue
+import time
+
+class ThreadPool(object):
+    def __init__(self, max_num):
+        self.StopEvent = 0#stop signal,if get task is StopEvent,Stop current task
+        self.q = Queue()
+        self.max_num = max_num  #maxiumum threads
+        self.terminal = False   #
+        self.created_list = [] #existed thread list
+        self.free_list = [] #free thread list
+        self.Daemon=False #set daemon thread
+
+    def run(self, func, args, callback=None):
+        """
+        :param func: task func
+        :param args: task func params
+        :param callback:
+        :return: if thread has terminated,return True or None
+        """
+
+        if len(self.free_list) == 0 and len(self.created_list) < self.max_num:
+            self.create_thread()
+        task = (func, args, callback,)
+        self.q.put(task)
+
+    def create_thread(self):
+        """
+        create thread
+        """
+        t = threading.Thread(target=self.call)
+        t.setDaemon(self.Daemon)
+        t.start()
+        self.created_list.append(t)#append current thread to list
+
+    def call(self):
+        """
+        get task in a loop and execute it
+        """
+        current_thread = threading.current_thread()   #get current thread·
+        event = self.q.get()    #get task from queue
+        while event != self.StopEvent:   #Determine whether task is a terminator
+
+            func, arguments, callback = event#get funcname,params,callback name
+            try:
+                result = func(*arguments)
+                func_excute_status =True#set func executed status success
+            except Exception as e:
+                func_excute_status = False#set func executed status failure
+                result =None
+                print('func executed error', e)
+
+            if func_excute_status: #
+                if callback is not None:#determine whetherif callback is None
+                    try:
+                        callback(result)
+                    except Exception as e:
+                        print(callback.__name__, e)
+
+
+            with self.worker_state(self.free_list,current_thread):
+                if self.terminal:
+                    event = self.StopEvent
+                else:
+                    event = self.q.get()
+
+        else:
+            self.created_list.remove(current_thread)
+
+    def close(self):
+        """
+        when all task finished,close all threads
+        """
+        full_size = len(self.created_list)#按已创建的线程数量往线程队列加入终止符。
+        while full_size:
+            self.q.put(self.StopEvent)
+            full_size -= 1
+
+    def terminate(self):
+        """
+        terminate thread
+        """
+        self.terminal = True
+        while self.created_list:
+            self.q.put(self.StopEvent)
+
+        self.q.empty()#清空任务队列
+
+    def join(self):
+        """
+        Blocking the thread pool context so that all threads can continue after execution
+        """
+        for t in self.created_list:
+            t.join()
+
+
+    @contextlib.contextmanager
+    def worker_state(self, state_list, worker_thread):
+        """
+        record the number of threads waiting in a thread
+        """
+        state_list.append(worker_thread)
+        try:
+            yield
+        finally:
+            state_list.remove(worker_thread)
+
+
+
+
+
+
+if __name__ == '__main__':
+    def Foo(arg):
+        return arg
+        # time.sleep(0.1)
+
+    def Bar(res):
+        print(res)
+
+    pool=ThreadPool(5)
+    # pool.Deamon=True#需在pool.run之前设置
+    for i in range(1000):
+        pool.run(func=Foo,args=(i,),callback=Bar)
+    pool.close()
+    pool.join()
+    # pool.terminate()
+
+    print("任务队列里任务数%s" %pool.q.qsize())
+    print("当前存活子线程数量:%d" % threading.activeCount())
+    print("当前线程创建列表:%s" %pool.created_list)
+    print("当前线程创建列表:%s" %pool.free_list)

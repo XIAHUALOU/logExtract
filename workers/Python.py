98528f474b2bc9dc63a1bf92ff3ccfbfa38fc16b
xiahualou <390306467@qq.com>
2019-08-30 10:18:03 +0800

    fix python

--- workers/Python.py
+++ workers/Python.py
@@ -1,3 +1,6 @@
+Test
+extra
+official
 # -*- encoding: utf-8 -*-
 """
 @Time    : 7/1/19 1:26 AM
@@ -12,34 +15,26 @@ class Python(BaseWorker):
         logs = self.read_from_file(mode=list)
         for log in logs:
             t, log = log
-            _b = 0
+            box = []
             try:
-                official = []
-                clear = []
                 for _ in log:
                     v = _.strip()
                     if v.startswith('Totals:'):
-                        _b += 1
                         v = v.split()[1:3]
                         v = [_[:-2] for _ in v]
-                        if len(official) == 0 and len(clear) == 0 and len(v) == 2:
-                            official = v
-                        if len(official) == 2 and len(clear) == 0 and len(v) == 2:
-                            clear = v
-                if len(official) == len(clear) == 2:
-                    self.merge(official)
-                    self.merge(clear)
-                else:
-                    self.failed(t, 'error logfile')
-                    self.merge(None)
-                    continue
-                if _b != 2:
+                        box.extend(v)
+                if len(box) != 4:
                     self.failed(t, 'error logfile')
                     self.merge(None)
                     continue
                 else:
+                    self.merge(box[0:2])
+                    self.merge(box[2:4])
                     self.status(t)
             except Exception as Ex:
                 self.failed(t, Ex)
                 self.merge(None)
                 continue
+
+    def to_excel(self):
+        pass
